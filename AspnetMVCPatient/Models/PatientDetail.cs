32ae45eb5f174db7afbfe6a36774964da50dc1ff
hehaibo@hotmail.com <hehaibo@hotmail.com>
2024-01-22 09:51:06 -0500

    Init checkin with patient page

---
+++ AspnetMVCPatient/Models/PatientDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspnetMVCPatient.Models
+{
+    public class PatientDetail
+    {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DOB { get; set; }
+    }
+}
---
+++ AspnetMVCPatient/Models/PatientMetricsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspnetMVCPatient.Models
+{
+    public class PatientMetricsViewModel
+    {
+        public PatientMetricsViewModel()
+        {
+
+        }
+        public IEnumerable<PatientDetail> Patients { get; set; }
+
+    }
+}
